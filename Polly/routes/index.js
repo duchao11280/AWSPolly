e9bd87887f601c1259e10d4d660f79434e8d251a
Thanhhoang112 <18110112@student.hcmute.edu.vn>
2021-12-02 17:05:12 +0700

    Change key Aws
    Đổi key để login

--- Polly/routes/index.js
+++ Polly/routes/index.js
@@ -8,9 +8,9 @@ var router = express.Router();
 //Tao client polly
 const Polly = new AWS.Polly({
 	region: 'us-east-1',
-	secretAccessKey: '2COvy0j3HiX8ofJkFHH0Tql+1CrREJIidowVheyA',
-	accessKeyId: 'ASIAQLGDQYLX2X3XYJHE',
-	sessionToken: 'FwoGZXIvYXdzEEkaDMkli4mpVbdoQecJBSLPARSnQxqsOugHaPxL0H1C4/e1iHbu+agkdw7+B3OAqBC71NFYrg1UHpOAKqg0YWzsHSr+Rsk7/DQT1O14zepY/QbP/BNjwTN4+sgzHzpW5EUG1/0jY4SxB6oaEFqhcgWWOQPd7rplA2uES7GLEdOqLAjSQb7TsugxbCBL5pZ0LxbzasvAfsqpdPOtCJ/9ZyUvNnlFUfnusYstJnqu4jb6ZcIBJXy8OEFiGj2x/+EfhkedIfMjE5Kn2+3KWVNuRSjcmssEPjSIe5i171xHARu5qijjz5yNBjItGNPcMc5LqfvoDgLt8/b0mnRdnoaLAaIojRQMbJ0sO//HBc39KXgL9LdXueLS'
+	secretAccessKey: 'IqOkhEsqlMwFVW9k7Fn6TcljROVkUU73rWsBFRTQ',
+	accessKeyId: 'ASIAQLGDQYLXY73PLOVU',
+	sessionToken: 'FwoGZXIvYXdzEGMaDM4pTC4vNo6P9k16TCLPAYiilhv+kAA6sPWRv5AUzghLtd9FCLqF655MYJaDgadzXEaOEh/5NsRhTHTsrRRjszfKjg8qlw4YQ5JvbrnjPzvmPYG1u5luZZFLNbKXkEIAOCl3iOcWOqPcH2I+S1UU//JC3Qsi/wphlVa42VqtPKJGbZr65IYSNjcxmYxB15TpqEJSpTz5M+jL4cQ1HEFh8UBZZEJzp4CujMH3yGU4+c6ERotioPNQOykDrrr+2XV7WC3HcmoSKpCvNt+aIJPbkDtdbZsWeelGLpcuy0sJ9yiCs6KNBjItXVYi9z7ED36L1sXm/y1r/FMCgOP1RomHGLJmfBAFwvEkBpz3juFLnKQOXa3Z'
 })
 
 /* GET home page. */
